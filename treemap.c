8efbd6c3f2c8068052f4626021291d1c809d3129
Your Name <you@example.com>
2023-09-15 15:44:31 -0300

    new try

--- treemap.c
+++ treemap.c
@@ -220,8 +220,17 @@ Pair * searchTreeMap(TreeMap * tree, void* key)
 }
 
 
-Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
+Pair * upperBound(TreeMap * tree, void* key) 
+{
+    if(searchTreeMap(tree,key) != NULL)
+    {
+        return tree -> current -> pair;
+    }
+    else
+    {
+        tree -> current = tree -> root;
+        TreeNode * ub_node = tree -> current;
+    }
 }
 
 Pair * firstTreeMap(TreeMap * tree) 
